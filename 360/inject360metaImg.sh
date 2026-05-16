c27261be992295ea99a1809616d8d1bde0486630
Rodrigo Polo <rodrigo.polo@gmail.com>
2026-05-16 04:26:47 -0600

    date update

--- 360/inject360metaImg.sh
+++ 360/inject360metaImg.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2024 Rodrigo Polo
+# Copyright (c) 2026 Rodrigo Polo
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
